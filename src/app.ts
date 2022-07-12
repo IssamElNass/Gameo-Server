b60f53abd90f831f371c6123cf0c2cc875a29b8c
IssamElNass <issamelnasri@gmail.com>
2022-07-12 04:16:50 +0200

    Added AUTHORIZATION header to be added in a request

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ class App {
 
       res.setHeader(
         "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method,Authorization,  Access-Control-Request-Headers"
       );
 
       // // set the CORS policy
